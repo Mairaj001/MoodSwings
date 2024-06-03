ff642047c5c61f52959a3f61544d77823c6f3e63
Mairaj001 <107746927+Mairaj001@users.noreply.github.com>
2024-06-03 16:56:33 +0500

    commit

--- app/src/main/java/com/example/moodswings/UserProflieFragment.java
+++ app/src/main/java/com/example/moodswings/UserProflieFragment.java
@@ -1,20 +1,42 @@
 package com.example.moodswings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.zip.Inflater;
 
 public class UserProflieFragment extends Fragment {
+
+    Button logout;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       return inflater.inflate(R.layout.user_profile_fragment,container,false);
+      View view= inflater.inflate(R.layout.user_profile_fragment,container,false);
+
+      this.logout=view.findViewById(R.id.btnLogout);
+
+      this.logout.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              FirebaseAuth.getInstance().signOut();
+              Toast.makeText(getContext(),"Logout",Toast.LENGTH_SHORT).show();
+              startActivity( new Intent(getContext(), ActivityLogin.class));
+              requireActivity().finish();
+          }
+      });
+
+
+      return view;
     }
 }
